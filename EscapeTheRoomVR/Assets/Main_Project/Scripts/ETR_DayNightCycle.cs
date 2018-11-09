d44e344cb3d90ad9ae17c1548232ac2935feed54
Yuqi Wang <8285693+yuqiw18@users.noreply.github.com>
2018-11-09 23:13:23 +0000

    VR interaction added

---
+++ EscapeTheRoomVR/Assets/Main_Project/Scripts/ETR_DayNightCycle.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ETR_DayNightCycle : MonoBehaviour {
+
+    private bool isNight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //float currentIntensity = this.gameObject.GetComponent<Light>().intensity;
+        float rotateDegree = 12f * Time.deltaTime;
+        this.gameObject.transform.Rotate(rotateDegree, 0, 0);
+        /*
+        if (currentIntensity >= 1) {
+            isNight = false;
+        }else if(currentIntensity <= 0)
+        {
+            isNight = true;
+        }
+
+        if (isNight)
+        {
+            float newIntensity = currentIntensity += (1.0f / 180.0f) * Time.deltaTime;
+            if (newIntensity >= 1)
+            {
+                this.gameObject.GetComponent<Light>().intensity = 1;
+            }
+            else
+            {
+                this.gameObject.GetComponent<Light>().intensity = newIntensity;
+            }
+        }
+        else {
+            float newIntensity = currentIntensity -= (1.0f / 180.0f) * Time.deltaTime;
+            if (newIntensity <= 0)
+            {
+                this.gameObject.GetComponent<Light>().intensity = 0;
+            }
+            else
+            {
+                this.gameObject.GetComponent<Light>().intensity = newIntensity;
+            }
+
+        }
+        Debug.Log(currentIntensity);
+        
+	}
+    */
+    }
+}
