e3e54163d8f797768e9aaa998d8067e47c51f295
Yuqi Wang <8285693+yuqiw18@users.noreply.github.com>
2018-11-11 02:04:58 +0000

    Light & fan switch added

---
+++ EscapeTheRoomVR/Assets/Main_Project/Scripts/ETR_FanSwitch.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using VRTK;
+using UnityEngine;
+
+// Algorithm developed by Fei Huang
+// Modified and implemented for VR by Yuqi Wang
+
+public class ETR_FanSwitch : MonoBehaviour {
+
+    public GameObject fan;
+    public bool isOn;
+    public float currentSpeed, maxSpeed, accelerateSpeed;
+    public AudioSource switchSoundEffect;
+    public AudioSource fanSoundEffect;
+    private VRTK_InteractableObject interactableObject;
+
+    public void Start()
+    {
+    }
+
+    protected virtual void OnEnable()
+    {
+        interactableObject = GetComponent<VRTK_InteractableObject>();
+        interactableObject.InteractableObjectUsed += InteractableObjectUsed;
+        interactableObject.InteractableObjectUnused += InteractableObjectUnused;
+    }
+
+    protected virtual void OnDisable()
+    {
+        interactableObject.InteractableObjectUsed -= InteractableObjectUsed;
+        interactableObject.InteractableObjectUnused -= InteractableObjectUnused;
+    }
+
+    protected virtual void Update()
+    {
+        if (isOn)
+        {
+            // When the fan is on
+            if (currentSpeed < maxSpeed)
+            {
+                currentSpeed += accelerateSpeed * Time.deltaTime;
+            }
+            else
+            {
+                currentSpeed = maxSpeed;
+            }
+        }
+        else
+        {
+
+            // When the fan is off
+            if (currentSpeed > 0)
+            {
+                currentSpeed -= accelerateSpeed / 2 * Time.deltaTime;
+            }
+            else
+            {
+                currentSpeed = 0;
+            }
+
+        }
+
+        fan.gameObject.transform.Rotate(0, currentSpeed, 0);
+    }
+
+    protected virtual void InteractableObjectUsed(object sender, InteractableObjectEventArgs e)
+    {
+        Debug.Log("Triggered");
+        SwitchFan();
+        if (switchSoundEffect != null)
+        {
+            switchSoundEffect.Play();
+        }
+        float degree = this.gameObject.transform.localEulerAngles.y;
+        Debug.Log(degree);
+        this.gameObject.transform.localEulerAngles = new Vector3(0, -degree, 0);
+        interactableObject.enabled = false;
+    }
+
+    protected virtual void InteractableObjectUnused(object sender, InteractableObjectEventArgs e)
+    {
+    }
+
+    private void SwitchFan()
+    {
+        if (isOn)
+        {
+            isOn = false;
+        }
+        else
+        {
+            isOn = true;
+        }
+    }
+
+}
---
+++ EscapeTheRoomVR/Assets/Main_Project/Scripts/ETR_LightSwitch.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using VRTK;
+using UnityEngine;
+
+// Coded by Yuqi Wang
+
+public class ETR_LightSwitch : MonoBehaviour
+{
+    //public HoverButton hoverButton;
+    public GameObject[] lightList;
+    public AudioSource switchSoundEffect;
+    private VRTK_InteractableObject interactableObject;
+
+    public void Start()
+    {
+        int lightNumber = lightList.Length;
+        
+    }
+
+    protected virtual void OnEnable()
+    {
+        interactableObject = GetComponent<VRTK_InteractableObject>();
+        interactableObject.InteractableObjectUsed += InteractableObjectUsed;
+        interactableObject.InteractableObjectUnused += InteractableObjectUnused;
+    }
+
+    protected virtual void OnDisable()
+    {
+        interactableObject.InteractableObjectUsed -= InteractableObjectUsed;
+        interactableObject.InteractableObjectUnused -= InteractableObjectUnused;
+    }
+
+    protected virtual void Update()
+    {}
+
+    protected virtual void InteractableObjectUsed(object sender, InteractableObjectEventArgs e)
+    {
+        Debug.Log("Triggered");
+        SwitchLight();
+        if (switchSoundEffect != null)
+        {
+            switchSoundEffect.Play();
+        }
+        float degree = this.gameObject.transform.localEulerAngles.y;
+        Debug.Log(degree);
+        this.gameObject.transform.localEulerAngles = new Vector3(0, -degree , 0);
+        interactableObject.enabled = false;
+    }
+
+    protected virtual void InteractableObjectUnused(object sender, InteractableObjectEventArgs e)
+    {
+        Debug.Log("NOT Triggered");
+    }
+
+    private void SwitchLight()
+    {
+        int lightNumber = lightList.Length;
+
+        if (lightNumber != 0) {
+
+            for (int i = 0; i < lightNumber; i++) {
+
+                if (!lightList[i].activeSelf)
+                {
+                    lightList[i].SetActive(true);
+                }
+                else {
+                    lightList[i].SetActive(false);
+                }
+
+            }
+
+        }
+    }
+
+}
