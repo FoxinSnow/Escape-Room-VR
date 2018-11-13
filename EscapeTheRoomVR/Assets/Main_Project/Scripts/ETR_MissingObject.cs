4681f822db4afb965a42f167a5df669032e3fb86
Yuqi Wang <8285693+yuqiw18@users.noreply.github.com>
2018-11-13 11:12:05 +0000

    Radio button

---
+++ EscapeTheRoomVR/Assets/Main_Project/Scripts/ETR_MissingObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ETR_MissingObject : MonoBehaviour {
+
+    public GameObject objectToReveal;
+    public GameObject objectToMatch;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == objectToMatch.gameObject) {
+            objectToReveal.SetActive(true);
+            other.gameObject.SetActive(false);
+            Destroy(this);
+        }
+    }
+}
