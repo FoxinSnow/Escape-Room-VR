feb81f791b80a312c29da8fe6dc0a9c62fe167be
Yuqi Wang <8285693+yuqiw18@users.noreply.github.com>
2018-11-14 12:54:05 +0000

    Chalk

---
+++ EscapeTheRoomVR/Assets/Main_Project/Scripts/ETR_RadioControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using VRTK;
+using UnityEngine;
+
+public class ETR_RadioControl : MonoBehaviour {
+
+    public GameObject popButton, backwardButton, playButton, forwardButton, stopButton;
+
+    public GameObject voulmeRotator, mainSwitch;
+
+    private VRTK_InteractableObject interactableObject;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
