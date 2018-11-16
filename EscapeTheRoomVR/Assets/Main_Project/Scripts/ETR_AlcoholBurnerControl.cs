92a9ca47980429907da48210db87a4dea0524b2e
Yuqi Wang <8285693+yuqiw18@users.noreply.github.com>
2018-11-16 19:35:16 +0000

    Burner

---
+++ EscapeTheRoomVR/Assets/Main_Project/Scripts/ETR_AlcoholBurnerControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using VRTK;
+using UnityEngine;
+
+public class ETR_AlcoholBurnerControl : MonoBehaviour {
+
+    private VRTK_InteractableObject burnerControl;
+
+    // Use this for initialization
+    void Start () {
+        burnerControl = GetComponent<VRTK_InteractableObject>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ EscapeTheRoomVR/Assets/Main_Project/Scripts/ETR_ClampRotator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using VRTK;
+using VRTK.Controllables.ArtificialBased;
+using UnityEngine;
+
+// Coded by Yuqi Wang
+public class ETR_ClampRotator : MonoBehaviour {
+
+    public Transform clamp;
+    private VRTK_ArtificialRotator clampInteraction;
+
+    private void Start()
+    {
+        clampInteraction = this.GetComponent<VRTK_ArtificialRotator>();
+    }
+
+    protected virtual void Update()
+    {
+        clamp.localPosition = new Vector3(-0.2345288f + (clampInteraction.GetValue() / -1080f) * -0.15f, clamp.localPosition.y, clamp.localPosition.z);
+    }
+
+}
