8ef357c7aa031510a3855eca7dc576c25907abc6
Yuqi Wang <8285693+yuqiw18@users.noreply.github.com>
2018-11-17 15:55:19 +0000

    exit

--- EscapeTheRoomVR/Assets/Main_Project/Scripts/ETR_AlcoholBurnerControl.cs
+++ EscapeTheRoomVR/Assets/Main_Project/Scripts/ETR_AlcoholBurnerControl.cs
@@ -5,15 +5,47 @@ using UnityEngine;
 
 public class ETR_AlcoholBurnerControl : MonoBehaviour {
 
+    public GameObject fireEffect;
     private VRTK_InteractableObject burnerControl;
 
     // Use this for initialization
     void Start () {
-        burnerControl = GetComponent<VRTK_InteractableObject>();
+        fireEffect.SetActive(false);
     }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
+    protected virtual void OnEnable()
+    {
+        burnerControl = this.GetComponent<VRTK_InteractableObject>();
+        burnerControl.InteractableObjectUsed += InteractableObjectUsed;
+        burnerControl.InteractableObjectUnused += InteractableObjectUnused;
+    }
+
+    protected virtual void OnDisable()
+    {
+        burnerControl.InteractableObjectUsed -= InteractableObjectUsed;
+        burnerControl.InteractableObjectUnused -= InteractableObjectUnused;
+    }
+
+    protected virtual void InteractableObjectUsed(object sender, InteractableObjectEventArgs e)
+    {
+        if (fireEffect.activeSelf)
+        {
+            fireEffect.SetActive(false);
+        }
+        else {
+            fireEffect.SetActive(true);
+        }
+        burnerControl.enabled = false;
+    }
+
+
+    protected virtual void InteractableObjectUnused(object sender, InteractableObjectEventArgs e)
+    {
+    }
 }
---
+++ EscapeTheRoomVR/Assets/Main_Project/Scripts/ETR_ExitDoor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using VRTK;
+using UnityEngine;
+
+public class ETR_ExitDoor : MonoBehaviour {
+
+    public Transform door;
+    public AudioSource doorSoundEffect;
+    private VRTK_InteractableObject doorKnobInteraction;
+
+    private bool isOpen;
+    // Use this for initialization
+    void Start () {
+        isOpen = false;
+    }
+
+    protected virtual void OnEnable()
+    {
+        doorKnobInteraction = GetComponent<VRTK_InteractableObject>();
+        doorKnobInteraction.InteractableObjectUsed += InteractableObjectUsed;
+        doorKnobInteraction.InteractableObjectUnused += InteractableObjectUnused;
+    }
+
+    protected virtual void OnDisable()
+    {
+        doorKnobInteraction.InteractableObjectUsed -= InteractableObjectUsed;
+        doorKnobInteraction.InteractableObjectUnused -= InteractableObjectUnused;
+    }
+
+    protected virtual void Update()
+    {
+
+    }
+
+    protected virtual void InteractableObjectUsed(object sender, InteractableObjectEventArgs e)
+    {
+        isOpen = true;
+        if (doorSoundEffect != null)
+        {
+            doorSoundEffect.Play();
+        }
+        doorKnobInteraction.enabled = false;
+    }
+
+    protected virtual void InteractableObjectUnused(object sender, InteractableObjectEventArgs e)
+    {
+    }
+
+}
