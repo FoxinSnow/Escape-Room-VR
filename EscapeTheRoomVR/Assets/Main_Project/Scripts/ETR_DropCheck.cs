cfb3fc6012f47b20212078f748a60c66a74e2d0a
Fox <huangfei037@hotmail.com>
2018-12-03 14:45:11 +0000

    add lock drop sound effect

---
+++ EscapeTheRoomVR/Assets/Main_Project/Scripts/ETR_DropCheck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ETR_DropCheck : MonoBehaviour {
+
+    public AudioSource lockDrop; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(this.gameObject.activeSelf.Equals(true)){
+            if(collision.gameObject.name.Equals("Floor_Attic")){
+                lockDrop.Play();
+                Debug.Log("Play lock drop sound effect.");
+            }
+        }
+    }
+}
