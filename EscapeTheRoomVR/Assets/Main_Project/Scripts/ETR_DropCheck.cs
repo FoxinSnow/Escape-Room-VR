2f4415881da61d103c9a519237d82926f1e8805b
Fox <huangfei037@hotmail.com>
2018-12-03 15:25:59 +0000

    lock drop update

--- EscapeTheRoomVR/Assets/Main_Project/Scripts/ETR_DropCheck.cs
+++ EscapeTheRoomVR/Assets/Main_Project/Scripts/ETR_DropCheck.cs
@@ -4,24 +4,35 @@ using UnityEngine;
 
 public class ETR_DropCheck : MonoBehaviour {
 
-    public AudioSource lockDrop; 
+    public AudioSource lockDrop;
+    private Vector3 dropSpeed;
+    private float speedDiff;
+    private bool drop = false;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        dropSpeed = this.GetComponent<Rigidbody>().velocity;
+        speedDiff = 0;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (drop.Equals(false))
+        {
+            dropSpeed = this.GetComponent<Rigidbody>().velocity;
+            speedDiff = Mathf.Abs(speedDiff - dropSpeed.y);
+            //Debug.Log(speedDiff);
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(this.gameObject.activeSelf.Equals(true)){
-            if(collision.gameObject.name.Equals("Floor_Attic")){
+
+            if(collision.gameObject.name.Equals("Floor_Attic") && speedDiff > 30){
                 lockDrop.Play();
                 Debug.Log("Play lock drop sound effect.");
+                drop = true;
             }
         }
     }
