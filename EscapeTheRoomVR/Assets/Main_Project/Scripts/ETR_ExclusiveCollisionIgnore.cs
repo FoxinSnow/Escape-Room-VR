6a61d69f0eaa732a3dfb38ef6095ff17a4b4b207
Yuqi Wang <8285693+yuqiw18@users.noreply.github.com>
2018-12-04 01:16:24 +0000

    Create ETR_ExclusiveCollisionIgnore.cs

---
+++ EscapeTheRoomVR/Assets/Main_Project/Scripts/ETR_ExclusiveCollisionIgnore.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Coded by Yuqi Wang
+public class ETR_ExclusiveCollisionIgnore : MonoBehaviour {
+
+    public string gameObjectTag;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.tag == gameObjectTag) {
+            Physics.IgnoreCollision(collision.collider, this.gameObject.GetComponent<Collider>());
+        }
+    }
+}
