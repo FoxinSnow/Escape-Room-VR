a19da7876ddbd752b56428c7f1a0c96adc0cf752
Yuqi Wang <8285693+yuqiw18@users.noreply.github.com>
2018-12-04 17:35:21 +0000

    UIControl (draft)

---
+++ EscapeTheRoomVR/Assets/ETR_UIControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ETR_UIControl : MonoBehaviour {
+
+    public GameObject subtitle;
+    public float subtitileFade;
+    private bool isShowing;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isShowing) {
+
+
+            // subtitle value ++
+
+            if (subtitileFade == 1.0f) {
+                isShowing = false;
+            }
+
+        }
+	}
+
+    void DisplaySubtitle(string info) {
+
+
+
+        isShowing = true;
+    }
+
+
+
+}
